ee85266fa02d5424ecd69b9bfc70009cd6212e4a
Nauvalshandika <shandikanauval@gmail.com>
2024-10-11 10:09:10 +0700

    Update 24104410003_M NAUVAL FITRA S_Loop.cpp

--- 24104410003_M NAUVAL FITRA S_Loop.cpp
+++ 24104410003_M NAUVAL FITRA S_Loop.cpp
@@ -10,17 +10,17 @@ int main()
     int a; "Masukkan jumlah data: ";
     cin >> a;
 
-    int jumlahGenap = 0;
     int jumlahGanjil = 0;
+    int jumlahGenap = 0;
 
     for (int i = 1; i < a; i++) {
-        if (i % 2 == 0) {
-            jumlahGenap += i;
-        }
-        else {
+        if (i % 2 == 1) {
             jumlahGanjil += i;
         }
+        else {
+            jumlahGenap += i;
+        }
     }
-    cout << "Jumlah total bilangan genap: " << jumlahGenap << endl;
     cout << "Jumlah total bilangan ganjil: " << jumlahGanjil << endl;
+    cout << "Jumlah total bilangan genap: " << jumlahGenap << endl;
 }
